52574e0666be86c733947ad6e9599d6f8c1b69f0
Abhay deep <abhay9456234282@gmail.com>
2024-10-20 14:39:57 +0530

    added session 3

---
+++ session 3/app.js
@@ -0,0 +1,24 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+const heading=React.createElement("h1",{
+    id:"hy",
+},"hello inside react");
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+
+//to create heirarchy like this
+//     <div id="container">
+//     <h1></h1>
+
+//     <h2></h2>
+//    </div>
+
+const heading1=React.createElement("h1",{id:"heading1"},"heading1");
+const heading2=React.createElement("h2",{id:"heading2"},"heading2");
+
+const container=React.createElement("container",{id:"container"},[heading1,heading2]);
+const root2=ReactDOM.createRoot(document.getElementById("root2"));
+root2.render(container);
+
+
